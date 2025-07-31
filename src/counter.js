d0ce43f5e160d49e09f5b1adca4542f5d9f33b46
Добровольский <kdobrovolsky02@gmail.com>
2025-07-31 13:17:19 +0300

    Task 1 codewars

--- src/counter.js
+++ src/counter.js
@@ -1,9 +1,15 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    // Задача 1.
+    // Вернуть количество гласных в заданной строке.Мы будем считать гласными буквы a, e, i, o, u (но не y).
+    // Строка может состоять только из букв в нижнем регистре и/или пробелов.
+
+    function getCount(str) {
+        const vowels = str.match(/[aeiou]/gi)
+        return vowels ? vowels.length : 0
+    }
+    //Разбор: сначала мы регулярным выраженим ищем один символ из перечисленных, далее с помощью match проверяем, если есть совпадение,
+    // то возвращаем найденный массив гластных. Если массив существует => возвращаем его длину, если null => 0
+
+    console.log(getCount('Helloooooo world, my name is Kirill'))
+
 }
