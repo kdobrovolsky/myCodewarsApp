33f58e910a4208c38833746e6b6d2e4fa68ea315
Добровольский <kdobrovolsky02@gmail.com>
2025-08-07 10:46:43 +0300

    6

--- src/counter.js
+++ src/counter.js
@@ -64,6 +64,20 @@ export function setupCounter(element) {
         if (v <= 0) return 0;
         return distance / v;
     }
-    console.log(sakuraFall(5))
+
+    // Задача 6
+    // Доработайте функцию/метод так, чтобы она возвращала URL-адрес, удаляя всё, что находится после символа # (якоря).
+    //     Примеры:
+    // "www.codewars.com#about" → "www.codewars.com"
+    // "www.codewars.com?page=1" → "www.codewars.com?page=1"
+
+    function removeUrlAnchor(url) {
+        const anchorIndex = url.indexOf('#');
+        return anchorIndex === -1 ? url : url.substring(0, anchorIndex);
+    }
+
+
+
+    console.log(removeUrlAnchor("www.codewars.com#about"))
 
 }
