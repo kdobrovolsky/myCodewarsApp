6d4b10dd04ae095f5a0390da23bf23f04c60767e
Добровольский <kdobrovolsky02@gmail.com>
2025-09-11 09:43:17 +0300

    Task 23,24

--- src/counter.js
+++ src/counter.js
@@ -282,7 +282,45 @@ export function setupCounter(element) {
         return arr[0]
     }
 
-    console.log(findSmallestInt([34, 15, 88, 2,-20]))
+    //Задача 23
+    // Банкоматы позволяют использовать PIN-коды длиной ровно 4 или 6 цифр, и PIN-коды не могут содержать ничего, кроме цифр.
+    //     Если функции передана допустимая строка PIN-кода, верните true, иначе верните false.
+    // Примеры (Ввод --> Вывод):
+    // "1234" --> true
+    // "12345" --> false
+    // "a234" --> false
+
+    function validatePIN (pin) {
+        return /^(\d{4}|\d{6})$/.test(pin)
+    }
+
+    //Задача 24
+    // Камень, Ножницы, Бумага
+    // Давайте играть! Вы должны вернуть, какой игрок выиграл! В случае ничьи верните "Ничья!".
+    // Примеры (Ввод1, Ввод2 --> Вывод):
+    // "scissors", "paper" --> "Player 1 won!" ("Игрок 1 выиграл!")
+    // "scissors", "rock" --> "Player 2 won!" ("Игрок 2 выиграл!")
+    // "paper", "paper" --> "Draw!" ("Ничья!")
+    // Примечание: В оригинале используются английские названия:
+    // rock (камень)
+    // paper (бумага)
+    // scissors (ножницы)
+
+    const rps = (p1, p2) => {
+        if (p1 === p2) {
+            return "Draw!";
+        } else if (
+            (p1 === 'rock' && p2 === 'scissors') ||
+            (p1 === 'scissors' && p2 === 'paper') ||
+            (p1 === 'paper' && p2 === 'rock')
+        ) {
+            return "Player 1 won!";
+        } else {
+            return "Player 2 won!";
+        }
+    };
+
+   console.log(rps('scissors','rock'));
 }
 
 
