b9f2e34ebc5190f8e03da1a1d8ed1455048f25a6
Добровольский <kdobrovolsky02@gmail.com>
2025-09-22 10:22:12 +0300

    Task 39

--- src/counter.js
+++ src/counter.js
@@ -931,7 +931,22 @@ const promise = new Promise((resolve, reject) => {
     return Number(inputString[0])
     }
 
-    console.log(getAge('1 year old'))
+    //Задача 39
+    // Создайте функцию, которая принимает массив имен и возвращает массив, в котором каждое имя записано с заглавной первой буквы,
+    // а остальные буквы — строчными.
+    //     Примеры:
+    // ["jo", "nelson", "jurie"] --> ["Jo", "Nelson", "Jurie"]
+    //     ["KARLY", "DANIEL", "KELSEY"] --> ["Karly", "Daniel", "Kelsey"]
+
+    function capMe(names) {
+    return names.map((name) => {
+        return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+    })
+
+
+    }
+
+    console.log(capMe(["jo", "NELson", "jurie"]))
 }
 
 
