39503494d3aedf5e15c77bf04077c301c044d5cb
Добровольский <kdobrovolsky02@gmail.com>
2025-09-30 10:19:05 +0300

    Task 47-48

--- src/counter.js
+++ src/counter.js
@@ -1077,7 +1077,38 @@ const promise = new Promise((resolve, reject) => {
         return a.filter(item => !set.has(item));
     }
 
-    console.log(arrayDiff([1, 2, 2, 2, 3],[2]))
+
+    //Задача 47
+    // Нам нужно удалить все, что идет после символа # (якоря) в URL, включая сам символ #.
+    // Примеры:
+    //     "www.codewars.com#about" → "www.codewars.com"
+    // "www.codewars.com?page=1" → "www.codewars.com?page=1" (остается без изменений, так как нет #)
+
+    function removeUrlAnchor(url){
+    return url.split('#')[0]
+    }
+
+    //Задача 48
+    // Напишите функцию, которая принимает список строк в качестве аргумента и возвращает отфильтрованный список, содержащий те же элементы, но с удаленными "гусями".
+    // "Гуси" — это любые строки из следующего массива, который предопределен в вашем решении:
+    //     javascript
+    //         ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+    // Например, если этот массив был передан как аргумент:
+    //     javascript
+    //         ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+    // Ваша функция должна вернуть следующий массив:
+    //     javascript
+    //         ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+    // Элементы в возвращаемом массиве должны быть в том же порядке, что и в исходном массиве, переданном в вашу функцию, но с удаленными "гусями".
+    //     Обратите внимание, что все строки будут в том же регистре, что и предоставленные, и некоторые элементы могут повторяться.
+
+    function gooseFilter (birds) {
+        let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(bird => !geese.includes(bird) )
+
+    }
+
+    console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]))
 }
 
 
