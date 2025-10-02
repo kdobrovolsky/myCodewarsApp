c1a011a2b6c9ea680562e70a2aefcf22c4b0dc85
Добровольский <kdobrovolsky02@gmail.com>
2025-10-02 10:43:15 +0300

    Task 52,53

--- src/counter.js
+++ src/counter.js
@@ -1163,7 +1163,73 @@ const promise = new Promise((resolve, reject) => {
        return 1 + persistence(res)
     }
 
-    console.log(persistence(39))
+
+
+    //Задача 52
+    // Напишите функцию, которая принимает массив из 10 целых чисел (от 0 до 9) и возвращает строку этих чисел в формате номера телефона.
+    //     Пример
+    // text
+    // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => возвращает "(123) 456-7890"
+    // Полученный формат должен быть правильным для выполнения этой задачи.
+    //     Не забудьте пробел после закрывающей скобки!
+
+    function createPhoneNumber(numbers){
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+    }
+
+    function createPhoneNumber2(numbers){
+        return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`;
+    }
+
+    //Задача 53
+    // Вам будет дан массив чисел. Вы должны отсортировать нечетные числа в порядке возрастания, оставив четные числа на их исходных позициях.
+    //     Примеры
+    // text
+    //     [7, 1]  =>  [1, 7]
+    //     [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+    //     [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+    // Пояснение:
+    //     Нечетные числа сортируются по возрастанию
+    // Четные числа остаются на тех же позициях, где они были изначально
+    // Порядок четных чисел не меняется
+    // Меняются только позиции нечетных чисел, которые теперь идут в отсортированном порядке
+
+
+    function sortArray(array) {
+     const oddNum = array.filter(item => item % 2 === 1).sort((a, b) => a - b)
+        let index = 0
+        return array.map(t=> {
+            if(t %2 !== 0 ){
+                return oddNum[index++];
+            }
+            return t
+        })
+    }
+
+    //Доп задача
+    // Замени все простые числа в массиве на букву 'P', а составные оставь как есть.
+    //     Пример:
+    // javascript
+    //     [1, 2, 3, 4, 5, 6] → [1, 'P', 'P', 4, 'P', 6]
+    function replacePrimes(array) {
+        function isPrime(num) {
+            if (num < 2) return false;
+            for (let i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i === 0) return false;
+            }
+            return true;
+        }
+
+
+        return array.map(item => {
+            if (isPrime(item)) {
+                return 'P';
+            }
+            return item;
+        });
+    }
+
+    console.log(replacePrimes([1, 2, 3, 4, 5, 6]))
 
 }
 
