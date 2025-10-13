7849a975696abe3c3b00bc453945050c6ec131f2
Добровольский <kdobrovolsky02@gmail.com>
2025-10-13 11:12:19 +0300

    task 66

--- src/counter.js
+++ src/counter.js
@@ -1447,8 +1447,34 @@ const promise = new Promise((resolve, reject) => {
         return totalPeople;
     }
 
-    console.log(number([[10, 0], [3, 5], [5, 8]]))
 
+
+    //Задача 66
+    // Дан массив (длиной не менее 3, но потенциально очень большой), содержащий целые числа.
+    //     Массив либо полностью состоит из нечётных чисел, либо полностью из чётных чисел, за исключением одного целого числа N.
+    //     Напишите метод, который принимает массив в качестве аргумента и возвращает этот «выброс» N.
+    //     Примеры
+    //     [2, 4, 0, 100, 4, 11, 2602, 36] --> 11 (единственное нечётное число)
+    // [160, 3, 1719, 19, 11, 13, -21] --> 160 (единственное чётное число)
+
+    function findOutlier(integers){
+        let evenCount = 0;
+        let oddCount = 0;
+        let lastEven, lastOdd;
+
+        for(let num of integers){
+            if(num % 2 === 0){
+                evenCount++;
+                lastEven = num;
+            } else {
+                oddCount++;
+                lastOdd = num;
+            }
+        }
+
+        return evenCount === 1 ? lastEven : lastOdd;
+    }
+    console.log(findOutlier( [161, 3, 1719, 19, 11, 13, -22]))
 
 }
 
