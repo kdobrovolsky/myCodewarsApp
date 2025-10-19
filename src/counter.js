64d75f51868ef626da976039bec43fc5a6a49936
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-10-19 16:10:36 +0300

    Task 71

--- src/counter.js
+++ src/counter.js
@@ -1540,10 +1540,30 @@ const promise = new Promise((resolve, reject) => {
         return cleanedStr === cleanedStr.split('').reverse().join('');
     }
 
+    //Задача 71
+    // Напиши число в развернутой форме
+    // Вам будет дано число, и нужно вернуть его в виде строки в развернутой форме. Например:
+    // 12 --> "10 + 2"
+    // 45 --> "40 + 5"
+    // 70304 --> "70000 + 300 + 4"
+    // ПРИМЕЧАНИЕ: Все числа будут целыми и больше 0.
+
+    function expandedForm(num) {
+       if(num <= 10) return num
+        const arrNum = num.toString().split('')
+        const result = []
+        for(let i = 0; i < arrNum.length; i++){
+            if(arrNum[i] !== '0'){
+                const value = arrNum[i] + '0'.repeat(arrNum.length - i -1)
+                result.push(value)
+            }
+        }
+        return result.join(' + ')
+    }
 
 
 
-    console.log(palindromFunc('Was it a car or a cat I saw'))
+    console.log(expandedForm(100005))
 
 
 }
