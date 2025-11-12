756f608f24a87af1338246a2b794fa09cc091f84
Добровольский <kdobrovolsky02@gmail.com>
2025-11-12 11:24:30 +0300

    task 91-92

--- src/counter.js
+++ src/counter.js
@@ -1903,7 +1903,60 @@ const promise = new Promise((resolve, reject) => {
         return res === 26
     }
 
-    console.log(isPangram("The quick brown fox jumps over the lazy dog"))
+    //Задача 91
+    // Дополни метод, который принимает массив целых чисел и возвращает один из следующих вариантов:
+    // «yes, ascending» — если числа в массиве отсортированы в порядке возрастания
+    // «yes, descending» — если числа в массиве отсортированы в порядке убывания
+    // «no» — в противном случае
+    // Порядок не обязательно должен быть строгим: отсортированный массив может содержать последовательные дубликаты, например,
+    // [1, 1, 2, 3] считается отсортированным в порядке возрастания.
+    // Гарантируется, что всегда будет существовать единственный верный ответ. Если точнее:
+    // не будет массивов с менее чем 2 элементами
+    // не будет массивов, в которых все элементы равны
+
+    function isSortedAndHow(array) {
+        const ascendingArray = [...array].sort((a, b) => a - b).join('')
+        const descendingArray = [...array].sort((a, b) => b - a).join('')
+        const original = array.join('')
+
+        if(original === ascendingArray){
+            return 'yes, ascending'
+        }else if(original === descendingArray){
+            return 'yes, descending'
+        }else {
+            return 'no'
+        }
+    }
+
+    //Задача92
+    // Ваша задача — создать таблицу умножения N×N, где размер передается в параметре.
+    //     Например, когда заданный размер равен 3:
+    // text
+    // 1 2 3
+    // 2 4 6
+    // 3 6 9
+    // Для приведенного примера возвращаемое значение должно быть:
+    //     javascript
+    //         [[1,2,3],[2,4,6],[3,6,9]]
+    // Пояснение:
+    //     Нужно написать функцию, которая принимает число N и возвращает двумерный массив (матрицу) N×N, где элемент в i-й строке и j-м столбце равен (i+1) * (j+1).
+
+    function multiplicationTable(size) {
+        let table = []
+        for (let i = 0; i < size; i++){
+            let row =[]
+            for (let j = 0; j < size; j++){
+            let resultJ = (i+1)*(j+1)
+                row.push(resultJ)
+
+            }
+            table.push(row)
+        }
+            return table
+    }
+
+
+    console.log(multiplicationTable([[1,2,3],[2,4,6],[3,6,9]]))
 }
 
 
