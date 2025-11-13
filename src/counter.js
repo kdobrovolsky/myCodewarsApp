57f10d241fde493349ecdcb728a4f87c4e2becef
Добровольский <kdobrovolsky02@gmail.com>
2025-11-13 10:20:11 +0300

    task 93

--- src/counter.js
+++ src/counter.js
@@ -1955,8 +1955,25 @@ const promise = new Promise((resolve, reject) => {
             return table
     }
 
+    // Задача 93
+    // Задача: "Найди пропущенное число"
+    //
+    // Дан массив, содержащий числа от 1 до N (включительно), где одно число пропущено. Массив не отсортирован. Найди пропущенное число.
+    //
+    //     Пример:
+    //
+    // text
+    // Вход: [3, 7, 1, 2, 8, 4, 5]  // N = 8, пропущено 6
+    // Выход: 6
 
-    console.log(multiplicationTable([[1,2,3],[2,4,6],[3,6,9]]))
+    function findMissingNumber(arr) {
+        const n = arr.length + 1;
+        const expectedSum = n * (n + 1) / 2;
+        const actualSum = arr.reduce((sum, num) => sum + num, 0);
+        return expectedSum - actualSum;
+    }
+
+    console.log(findMissingNumber([3, 7, 1, 2, 8, 4, 5]))
 }
 
 
