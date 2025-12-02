4e9c14208ecf692e09c1493903f5622d6782ceae
Добровольский <kdobrovolsky02@gmail.com>
2025-12-02 16:23:03 +0300

    task 112

--- src/counter.js
+++ src/counter.js
@@ -2288,9 +2288,20 @@ const promise = new Promise((resolve, reject) => {
 
     const addLength = (str) => str.split(' ').map(s => `${s} ${s.length}`)
 
+    //Задача 112
+    // Возьмите 2 строки s1 и s2, содержащие только буквы от a до z.
+    //     Верните новую отсортированную строку (в алфавитном порядке по возрастанию),
+    // максимально возможной длины, содержащую уникальные буквы — каждая буква должна встречаться только один раз —
+    // которые присутствуют либо в s1, либо в s2.
 
+    function longest(s1, s2) {
+        const arrStr1 = s1.split('')
+        const arrStr2 = s2.split('')
+        const result = new Set(arrStr1.concat(arrStr2).sort())
+        return [...result].join('')
+    }
 
-    console.log(addLength('apple ban'))
+    console.log(longest('xyaabbbccccdefww' , 'xxxxyyyyabklmopq'))
 
 }
 
