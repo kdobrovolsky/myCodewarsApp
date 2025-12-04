79d19fcd163e75111aceceef0a92d70fd3ac2796
Добровольский <kdobrovolsky02@gmail.com>
2025-12-04 11:33:25 +0300

    filter elements divisible by their index

--- src/counter.js
+++ src/counter.js
@@ -2361,11 +2361,29 @@ export function setupCounter(element) {
         return result.join('')
     }
 
+    //Задача 114
+    // Вернуть новый массив, состоящий из элементов, которые кратны своему собственному индексу во входном массиве (длина > 1).
+    //     Некоторые примеры:
+    //     tex
+    //         [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+    //     [68, -1, 1, -7, 10, 10] => [-1, 10]
+    //     [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
 
+    function multipleOfIndex(array) {
+        const result = []
+        for (let i = 0; i < array.length; i++) {
+            if(i === 0) {
+                if(array[i] === 0) result.push(i)
+                continue
+            }
+            if(array[i] % i === 0){
+                result.push(array[i])
+            }
+        }
+        return result
+    }
 
-
-
-    console.log(encod2('hello'))
+    console.log(multipleOfIndex([0,2,3,6,9]))
 
 }
 
