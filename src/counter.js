d4762e5e6e2eeddbfa10b5d28400ad10a8c2bd2d
Добровольский <kdobrovolsky02@gmail.com>
2025-12-09 15:49:27 +0300

    this

--- src/counter.js
+++ src/counter.js
@@ -2505,7 +2505,6 @@ export function setupCounter(element) {
 
     const maxCount = (arr) => {
         let result = arr[0]
-
         for(let num of arr){
             if(num > result){
                 result = num
@@ -2514,7 +2513,65 @@ export function setupCounter(element) {
         return result;
     }
 
-    console.log(maxCount([1,3,6,9,8]))
+
+    //Задачи на тему this
+
+    //Написать counter
+    function Counter() {
+        this.value = 0
+
+        this.inc = function(){
+            this.value++
+        }
+        this.dec = function(){
+            this.value--
+        }
+
+        this.get = function(){
+           return  this.value
+        }
+    }
+    const counter = new Counter();
+
+    counter.inc();//1
+    counter.inc();//2
+    counter.inc();//3
+    counter.dec();//3-1 = 2
+    console.log(counter.get())
+
+    //Задание 2
+    //Какой вывод ?
+
+    var length = 10;
+
+    const obj = {
+        length: 5,
+        method() {
+            return this.length;
+        }
+    };
+
+    const array = [1, 2, obj.method];
+
+    //Какой вывод?
+    var name = "Global";
+
+    const obj = {
+        name: "Object",
+        getName() {
+            return this.name;
+        }
+    };
+
+    const fn = obj.getName;
+
+    console.log(obj.getName()); // object
+    console.log(fn()); // Globacl в нестрогом режиме из-за var, т.к он всплывает, если в строгом, то undefined
+
+
+    console.log(array[2]());
+
+
 
 
 
