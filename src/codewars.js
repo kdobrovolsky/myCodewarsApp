ecb8b35077d6dd30fa0bf6e21361d8dbcfdc1330
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-12-14 15:52:54 +0300

    Solve capitalize string kata

--- src/codewars.js
+++ src/codewars.js
@@ -2607,5 +2607,38 @@ export function setupCounter(element) {
     return result.join('.');
     }
 
-    console.log(abbrevName('Sam Harris'));
+    //Задача 128
+    // Данную строку. Заглавные буквы, которые занимают чётные индексы и нечётные индексы, обрабатываются отдельно, и возвращается результат, как показано ниже. Индекс 0 считается чётным.
+    //     Например, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. Смотрите тестовые примеры для дополнительных примеров.
+    //     Входные данные будут строкой в нижнем регистре без пробелов.
+
+    function capitalize1(s){
+        const arrS = s.split('');
+        let result1 = []
+        let result2 = []
+       for(let i=0; i<arrS.length; i++){
+           if(i % 2 === 0){
+               result1.push(arrS[i].toUpperCase())
+           }else {
+               result1.push(arrS[i].toLowerCase())
+           }
+       }
+       for(let i=0; i<arrS.length; i++){
+           if(i % 2 === 0){
+               result2.push(arrS[i].toLowerCase())
+           }else {
+               result2.push(arrS[i].toUpperCase())
+           }
+       }
+        return [result1.join(''), result2.join('')];
+    };
+
+
+    function capitalize(s){
+        const odd = s.split('').map((t,i)=> i % 2 ===0 ? t.toUpperCase() : t.toLowerCase()).join('');
+        const even = s.split('').map((t,i)=> i % 2 ===0 ? t.toLowerCase() : t.toUpperCase()).join('');
+        return [odd, even]
+    };
+
+    console.log(capitalize('AbRACADAbRA'));
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from './codewars.js'
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#codewars'))
