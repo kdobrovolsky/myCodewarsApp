6efe8b987453e1c4fb81fc9565227d5ef781247a
Добровольский <kdobrovolsky02@gmail.com>
2025-12-16 11:04:59 +0300

    longestWord now compares by length, not alphabetically

--- src/codewars.js
+++ src/codewars.js
@@ -2662,6 +2662,25 @@ export function setupCounter(element) {
 
     }
 
-    console.log(bump('nnnnnnnnn_________nnnnnnn'))
+    //Задача 129
+    // Дана строка со словами, разделёнными пробелами. Верните самое длинное слово.
+    //     Если есть несколько слов максимальной длины, верните самое правое из них.
+    //     Примеры:
+    // "red white blue" => "white"
+    // "red blue gold" => "gold"
+
+    function longestWords(stringOfWords) {
+       const arrStr = stringOfWords.split(' ');
+       let lonesy = ''
+        for(let str of arrStr){
+            if(str.length >= lonesy.length){
+                lonesy = str
+            }
+
+        }
+        return lonesy
+    }
+
+    console.log(longestWords("blue grey "))
 
 }
