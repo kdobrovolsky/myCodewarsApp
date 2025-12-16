8f4bbd23c6055c86c587b9bf1d6d98375d0468f5
Добровольский <kdobrovolsky02@gmail.com>
2025-12-16 11:14:52 +0300

    correct digit counting for negative numbers in multiply function

--- src/codewars.js
+++ src/codewars.js
@@ -2681,6 +2681,18 @@ export function setupCounter(element) {
         return lonesy
     }
 
-    console.log(longestWords("blue grey "))
+    //Задача 130
+    // Джеку очень нравится число пять: хитрость здесь в том, что нужно умножить каждое число на 5, возведённое в степень, равную количеству цифр в этом числе. Например:
+    // 3 --> 15 ( 3 * 5¹)
+    // 10 --> 250 ( 10 * 5²)
+    // 200 --> 25000 (200 * 5³)
+    // 0 --> 0 ( 0 * 5¹)
+    // -3 --> -15 ( -3 * 5¹)
+
+    function multiply(number){
+        return number * Math.pow(5, number > 0 ? number.toString().split('').length : number.toString().split('').length -1)
+    }
+
+    console.log(multiply(-3))
 
 }
