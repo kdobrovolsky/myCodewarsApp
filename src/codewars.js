a3e882caf069318004308857b706053389b2e93e
Добровольский <kdobrovolsky02@gmail.com>
2025-12-20 11:15:26 +0300

    Add vowelIndices function

--- src/codewars.js
+++ src/codewars.js
@@ -2737,7 +2737,30 @@ export function setupCounter(element) {
         return arr.slice(1,-1).join('')
     }
 
+    //Задача 134
+    // Мы хотим узнать индексы гласных букв в заданном слове. Например, в слове "super" две гласные буквы (вторая и четвертая буквы).
+    // Таким образом, для строки "super" мы должны вернуть список [2, 4].
+    //     Несколько примеров:
+    //     "Mmmm" => []
+    // "Super" => [2, 4]
+    // "Apple" => [1, 5]
+    // "YoMama" => [1, 2, 4, 6]
+    // ПРИМЕЧАНИЯ
+    // Гласными в данном контексте считаются: a, e, i, o, u, y (включая заглавные буквы).
+    // Индексация ведётся от 1 до n (не с нуля!).
 
-    console.log(arrayString('3'))
+        function vowelIndices(word){
+            const vowels = ['a', 'e', 'i', 'o', 'u', "y"]
+            const wordLowCase = word.toLowerCase()
+            let result = []
+         for(let i = 0; i < wordLowCase.length; i++){
+          if(vowels.includes(wordLowCase[i])){
+              result.push(i+1)
+          }
+         }
+         return result
+        }
+
+    console.log(vowelIndices('YoMama'))
 
 }
