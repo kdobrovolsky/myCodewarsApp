305668d6bc64e5dfbeb2c1559f82369896d45028
kdobrovolsky <kdobrovolsky02@gmail.com>
2026-01-09 13:40:41 +0300

    Switch a and b, keep c kata solution

--- src/codewars.js
+++ src/codewars.js
@@ -3071,9 +3071,28 @@ return result
     }
 
 
-    console.log(multiTable(5))
 
 
+    //Задача 150
+    // Дана строка, состоящая из букв a, b и/или c. Поменяйте местами буквы a и b (замените a на b и наоборот). Все вхождения буквы c оставьте без изменений.
+    //     Пример:
+    // 'acb' --> 'bca'
+    // 'aabacbaa' --> 'bbabcabb'
 
+    function switcheroo(x){
+    const letters = x.split('')
+        let result = []
+        for(let letter of letters){
+            if(letter === 'a'){
+                result.push('b')
+            }else if(letter === 'b'){
+                result.push('a')
+            }else {
+                result.push(letter)
+            }
+        }
+        return result.join('')
+    }
 
+    console.log(switcheroo('acb'))
 }
