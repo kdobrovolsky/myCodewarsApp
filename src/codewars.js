2d2b597bb978e3bacc23814a1713ebe11d9ab1c1
kdobrovolsky <kdobrovolsky02@gmail.com>
2026-01-17 21:23:55 +0300

    add onlyOne function for checking single true flag

--- src/codewars.js
+++ src/codewars.js
@@ -3294,6 +3294,32 @@ export function setupCounter(element) {
         }
     }
 
-    console.log(findMissingLetter(['O','Q','R','S']))
+    //Задача 159
+    // Переведи текст нан русский
+    // Задача: Написать функцию, которая возвращает True, если ТОЛЬКО ОДИН из булевых флагов равен True, в противном случае возвращает False.
+    // Если булевых флагов нет, возвращает False.
+    //
+    //     Ввод | Ожидаемый вывод
+    //     [] | False
+    //     [True, False, False] | True
+    //     [True, False, False, True] | False
+    //     [False, False, False, False] | False
+
+    function onlyOne(arr) {
+    if( !arr || !Array.isArray(arr)) return false
+    if( arr.length === 0 ) return false
+
+    let result = 0
+    for(let countTrue of arr){
+        if(countTrue){
+            result++
+        }
+    }
+    return result === 1
+
+    }
+
+
+    console.log(onlyOne([true, false, false ]))
 
 }
