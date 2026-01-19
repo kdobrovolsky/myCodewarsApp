dc066f2ce47052ca8a6303f7da1a7882c31f9213
Добровольский <kdobrovolsky02@gmail.com>
2026-01-19 14:03:13 +0300

    add highestRank function (frequency + max value logic)

--- src/codewars.js
+++ src/codewars.js
@@ -3362,8 +3362,28 @@ export function setupCounter(element) {
         return nums - strNum
     }
 
+    //Задача 163
+    // Завершите метод, который возвращает число, наиболее часто встречающееся в заданном входном массиве.
+    //     Если есть несколько чисел с одинаковой максимальной частотой, верните наибольшее из них.
+    //     Примечание: пустые массивы даваться не будут.
+    //     Примеры:
+    // [12, 10, 8, 12, 7, 6, 4, 10, 12] --> 12
+    //     [12, 10, 8, 12, 7, 6, 4, 10, 12, 10] --> 12
+    //     [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10] --> 3 (6ky)
 
+    function highestRank(arr){
+        let counts  = {}
 
-    console.log(divCon([1,'2','3',6]))
+        arr.forEach(i => {
+            counts[i] = (counts[i] || 0) +1
+        })
+        let maxValue = Math.max(...Object.values(counts))
+
+        let candidates = Object.keys(counts).filter(key => counts[key] === maxValue).map(t=> Number(t))
+
+        return candidates.sort((a, b) => b - a)[0]
+    }
+
+    console.log(highestRank([12, 10, 8, 12, 7, 10, 10, 12]))
 
 }
