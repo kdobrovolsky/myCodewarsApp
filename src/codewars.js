6d315a078306d915c1ec9fe8bb2b08e5d238a563
Добровольский <kdobrovolsky02@gmail.com>
2026-01-23 16:26:04 +0300

    implement String.prototype.camelCase

--- src/codewars.js
+++ src/codewars.js
@@ -3547,6 +3547,25 @@ export function setupCounter(element) {
 
     }
 
-    console.log(toNumberArray(["1", "2", "3"]))
+    //Задача 171
+    // Напишите метод (или функцию, в зависимости от языка), который преобразует строку в camelCase,
+    // то есть все слова должны начинаться с заглавной буквы, а пробелы должны быть удалены.
+    // Примеры (вход --> выход):
+    // "hello case" --> "HelloCase"
+    // "camel case word" --> "CamelCaseWord"
+    // Не забудьте оценить это ката! Спасибо :)
+
+    String.prototype.camelCase=function(){
+
+        if(this.length === 0) return ''
+        const arrStr = this.split(' ')
+        const result = []
+      for(let letter of arrStr){
+        result.push(letter[0].toUpperCase() + letter.slice(1))
+      }
+      return result.join('')
+    }
+
+   console.log('hello case'.camelCase())
 
 }
